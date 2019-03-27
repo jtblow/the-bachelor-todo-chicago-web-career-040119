a1fe2e91d10994491f6ea03f878c35066e24b0e8
Jake Bartlow <jtbartlow@gmail.com>
2019-03-27 19:36:31 +0000

    Automatically backed up by Learn

--- lib/bachelor.rb
+++ lib/bachelor.rb
@@ -13,7 +13,10 @@ end
 
 
 def get_contestant_name(data, occupation)
-  # code here
+  data[season][people].each do |k, v|
+    if v == occupation
+      return people["name"]
+    end
 end
 
 def count_contestants_by_hometown(data, hometown)
