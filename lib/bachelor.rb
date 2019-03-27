8cab1257e8b2c22931ffa637292e4cfba55fa89a
Jake Bartlow <jtbartlow@gmail.com>
2019-03-27 20:09:01 +0000

    Automatically backed up by Learn

--- lib/bachelor.rb
+++ lib/bachelor.rb
@@ -41,8 +41,10 @@ end
 
 def get_occupation(data, hometown)
  data.each. do |season, arr|
-   arr.each do |people|
-     people.
+   arr.each do |people, key|
+     key.find do |v|
+       v == hometown
+       return "occupation"[v]
 end
 
 def get_average_age_for_season(data, season)
